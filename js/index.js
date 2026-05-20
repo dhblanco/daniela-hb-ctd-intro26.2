559e53b0a649caa5d9fe62d4cfb0912968ab47c2
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-05-19 22:59:23 -0400

    removed manually created <footer> and created the element in index.js instead to follow AIRHUB suggestions

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
 let today = new Date()
 let thisYear = today.getFullYear()
 
-let footer = document.querySelector('footer')
+/* let footer = document.querySelector('footer') */
+    /* Instead of inserting footer manually via HTML using document.querySelector, I edited code to create footer using Javascript "in order to more closely the instructions suggested adding the footer via DOM methods." */
+ 
+let footer = document.createElement('footer')
 let copyRight = document.createElement('p')
+
+/* AIRHUB feedback suggested considering using innerText instead, instead of innerHTML to prevent unintended HTML parsing and improve clarity. However, in this case, this would result in the text is treated as literal text, so &copy; would be displayed exactly as written rather than being interpreted as the HTML entity for the copyright symbol. */
 copyRight.innerHTML = `&copy; ${thisYear} Daniela Hernández Blanco`
 
 footer.appendChild(copyRight)
+document.body.appendChild(footer)
 
 let skills = ["JavaScript", "HTML", "CSS", "GitHub"]
 let skillsList = document.querySelector("#Skills ul")
