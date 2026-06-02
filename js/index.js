315cb2e78f848f051af5b29fe7ce110b90a26e17
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-06-01 21:00:50 -0400

    appending the button to the message element

--- js/index.js
+++ js/index.js
@@ -45,20 +45,19 @@ messageForm.addEventListener('submit',function(event){
     
     let newMessage = document.createElement('li');
     messageList.appendChild(newMessage)
-    newMessage.innerHTML=`<a href="mailto:${email}">${name}</a> <span>${message}</span>`;
+    newMessage.innerHTML=`<a href="mailto:${email}">${name}</a>: <span>"${message}"</span>`;
 
     //Remove Button
     let removeButton = document.createElement("button")
     removeButton.setAttribute('type', 'button')
-
     removeButton.innerText = "remove"
-    removeButton.addEventListener('click',function(){
-   
-    let entry = removeButton.parentNode
-    entry.remove()
+
     newMessage.appendChild(removeButton)
 
-})
+    removeButton.addEventListener('click',function(){
+        let entry = removeButton.parentNode
+        entry.remove()
+    })
 })
 
 
