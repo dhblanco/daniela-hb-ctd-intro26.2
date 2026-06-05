c03a01fd6456d63f06b3e25f11c599944d48c50f
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-06-05 10:30:03 -0400

    exploring async/await function with CTD mentor (christian) to gather information from open ai and make progress towards creating li elements

--- js/openapi.js
+++ js/openapi.js
@@ -21,20 +21,41 @@ let teamsList = document.getElementById("teams")
   //CREATE LI ELEMENTS
   //APPEND TO UL "leagues" "teams"
 
-  fetch("https://v3.football.api-sports.io/leagues", requestOptions)
-  .then(response => response.json())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  // fetch("https://v3.football.api-sports.io/leagues", requestOptions)
+  // .then(response => response.json())
+  // .then(result => console.log(result))
+  // .catch(error => console.log('error', error));
 
-  teamsButton,Button.addEventListener("click", function(){
-    fetch("https://v3.football.api-sports.io/teams?league=1&season=2022", requestOptions)
-    .then(response => {
-      throw New Error (`Response status: ${response.status}`)
+  async function fetchRepos() {
+    try {
+        let response = await fetch("https://v3.football.api-sports.io/teams?league=1&season=2022", requestOptions)
+        
+        if(!response.ok){
+            throw new Error(response.status);
+        }
+
+        let data = await response.json()
+        console.log(data.response)
+        for (let i = 0; i < data.response.length; i++) {
+          const element = data.response[i]["team"].name;
+          console.log(element)
+          let name = document.createElement('li')
+        }
+    } catch (error) {
+        console.error('An eeror occurred',error)
     }
-    return response.json() //?? list of team names that played in 2022 world cup
-  })
+}
 
-  
+fetchRepos();
+
+  // teamsButton,Button.addEventListener("click", function(){
+  //   fetch("https://v3.football.api-sports.io/teams?league=1&season=2022", requestOptions)
+  //   .then(response => {
+  //     throw New Error (`Response status: ${response.status}`)
+  //   }
+  //   return response.json() //?? list of team names that played in 2022 world cup
+  // })
+
 
   //fetch('https://api.github.com/users/dhblanco/repos')
     // .then(response => {
