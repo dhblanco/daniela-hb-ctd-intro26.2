5ece4b186d0325723a3a1bdedc01cd227085d3a6
Daniela HB <267224774+dhblanco@users.noreply.github.com>
2026-06-04 21:06:30 -0400

    added id and event listeners for buttons

--- js/openapi.js
+++ js/openapi.js
@@ -1,5 +1,5 @@
 var myHeaders = new Headers();
-myHeaders.append("x-apisports-key", "XxXxXxXxXxXxXxXxXxXxXxXx");
+myHeaders.append("x-apisports-key", "005c9c26e8ecdb82edd97cdefecd5119");
 
 var requestOptions = {
   method: 'GET',
@@ -7,11 +7,15 @@ var requestOptions = {
   redirect: 'follow'
 };
 
+let leagueButton = document.getElementById("leagues-button")
+let teamsButton = document.getElementById("teams-button")
+
 fetch("https://v3.football.api-sports.io/leagues", requestOptions)
   .then(response => response.json())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
 
-fetch("https://v3.football.api-sports.io/teams?league=1&season=2022")
+  //ADD EVENT LISTENERS FOR CLICKING BUTTONS
 
-fetch("https://v3.football.api-sports.io/teams?league=1&season=2014")
+  leagueButton.addEventListener("click", fetch("https://v3.football.api-sports.io/teams?league=1&season=2022", requestOptions))
+  teamsButton.addEventListener("click", fetch("https://v3.football.api-sports.io/teams?league=1&season=2014", requestOptions))
